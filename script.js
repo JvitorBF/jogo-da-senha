2bb0b3d7dfe227e3e0778947ec8b4867ae10d443
João Vitor <jvitorbf35@gmail.com>
2023-04-04 19:19:49 -0300

    Comparação da senha com a escolha de cores do usuário feita, assim como a funcionalidade das bolinhas pequenas indicando erros e acertos

--- script.js
+++ script.js
@@ -6,13 +6,6 @@ canvas.height = window.innerHeight;
 // Seleciona o contexto 2D do canvas
 const ctx = canvas.getContext("2d");
 
-// Cria uma imagem e define sua fonte
-const image = new Image();
-image.src = "../jogoDaSenha/image/design.png";
-
-// button para verificar
-const btn = document.getElementById("btnConferir");
-
 // Define as cores disponíveis e o número de cores
 const cores_disponiveis = [
   "#ff0000",
@@ -24,7 +17,6 @@ const cores_disponiveis = [
   "#ff9999",
   "#0000ff",
 ];
-
 const num_cores = cores_disponiveis.length;
 
 // Define as bolinhas grandes iniciais e suas cores
@@ -35,11 +27,31 @@ const posicao_bolinhas_grandes = [
   { x: 350, y: 729, radius: 15, color: cores_disponiveis[3], colorIndex: 0 },
 ];
 
-// Espera a imagem carregar antes de desenhá-la no canvas
-image.onload = function () {
-  ctx.drawImage(image, 12, 20);
+// Cria uma imagem e define sua fonte
+const image = new Image();
+image.src = "../jogo da senha/image/design.png";
 
-  // Desenha as bolinhas grandes iniciais
+// Cria um array para guardar a senha aleatória gerada
+const senha = [];
+for (let i = 0; i < 4; i++) {
+  const indice = Math.floor(Math.random() * cores_disponiveis.length);
+  senha.push(cores_disponiveis[indice]);
+}
+
+// Função que retorna o índice da bolinha no vetor
+function circleIndex(circle) {
+  for (let i = 0; i < posicao_bolinhas_grandes.length; i++) {
+    if (
+      circle.x === posicao_bolinhas_grandes[i].x &&
+      circle.y === posicao_bolinhas_grandes[i].y
+    ) {
+      return i;
+    }
+  }
+}
+
+// Função que desenha as bolinhas grandes
+function desenharBolinhasGrandes() {
   posicao_bolinhas_grandes.forEach((circle) => {
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
@@ -47,6 +59,62 @@ image.onload = function () {
     ctx.fill();
     ctx.closePath();
   });
+}
+
+function desenharBolinhasPequenas(
+  primeira_linha,
+  cores_escolhidas_usuario,
+  senha
+) {
+  for (let i = 0; i < 4; i++) {
+    const circle = primeira_linha[i];
+    ctx.beginPath();
+
+    // Verifica se a cor e a posição estão corretas
+    if (senha[i] === cores_escolhidas_usuario[i]) {
+      ctx.fillStyle = "black";
+    }
+    // Verifica se apenas a cor está correta
+    else if (senha.includes(cores_escolhidas_usuario[i])) {
+      ctx.fillStyle = "green";
+    }
+    // Caso contrário, pinte a bolinha de cinza (ou qualquer outra cor que você desejar)
+    else {
+      ctx.fillStyle = "gray";
+    }
+
+    ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+function encontrarBolinhaClicada(event) {
+  return posicao_bolinhas_grandes.find((circle) => {
+    const mouseX = event.offsetX;
+    const mouseY = event.offsetY;
+    const distX = mouseX - circle.x;
+    const distY = mouseY - circle.y;
+    const distance = Math.sqrt(distX * distX + distY * distY);
+    return distance <= circle.radius;
+  });
+}
+
+function atualizarBolinhaGrande(clickedCircle) {
+  clickedCircle.colorIndex = (clickedCircle.colorIndex + 1) % num_cores;
+  clickedCircle.color = cores_disponiveis[clickedCircle.colorIndex];
+}
+
+function atualizarCoresEscolhidas(cores_escolhidas_usuario, clickedCircle) {
+  cores_escolhidas_usuario[circleIndex(clickedCircle)] = clickedCircle.color;
+}
+
+// Espera a imagem carregar antes de desenhá-la no canvas
+image.onload = function () {
+  ctx.drawImage(image, 12, 20);
+
+  // Desenha as bolinhas grandes iniciais
+  desenharBolinhasGrandes();
 
   // Inicio o vetor com as cores padrões
   const cores_escolhidas_usuario = [
@@ -59,147 +127,168 @@ image.onload = function () {
   // Adiciona um evento de clique para cada bolinha grande
   canvas.addEventListener("click", function (event) {
     // Encontra a bolinha clicada
-    const clickedCircle = posicao_bolinhas_grandes.find((circle) => {
-      const mouseX = event.offsetX;
-      const mouseY = event.offsetY;
-      const distX = mouseX - circle.x;
-      const distY = mouseY - circle.y;
-      const distance = Math.sqrt(distX * distX + distY * distY);
-      return distance <= circle.radius;
-    });
+    const clickedCircle = encontrarBolinhaClicada(event);
 
     if (clickedCircle) {
       // Atualiza a cor da bolinha grande clicada com a nova cor selecionada pelo usuário
-      clickedCircle.colorIndex = (clickedCircle.colorIndex + 1) % num_cores;
-      clickedCircle.color = cores_disponiveis[clickedCircle.colorIndex];
-      cores_escolhidas_usuario[circleIndex(clickedCircle)] =
-        clickedCircle.color;
-
-      console.log(cores_escolhidas_usuario);
+      atualizarBolinhaGrande(clickedCircle);
+      // Esta função é responsável por atualizar as cores escolhidas pelo usuário.
+      atualizarCoresEscolhidas(cores_escolhidas_usuario, clickedCircle);
       // Redesenha todas as bolinhas grandes
-      posicao_bolinhas_grandes.forEach((circle) => {
-        ctx.beginPath();
-        ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
-        ctx.fillStyle = circle.color;
-        ctx.fill();
-        ctx.closePath();
+      desenharBolinhasGrandes();
+    }
+  });
+
+  // button para verificar
+  const btn = document.getElementById("btnConferir");
+
+  btn.addEventListener("click", () => {
+    const primeira_linha = posicao_bolinhas_medias[0];
+
+    for (let i = 0; i < 4; i++) {
+      primeira_linha.forEach((circle, index) => {
+        if (index === i) {
+          ctx.beginPath();
+          if (circle.y > 645) {
+            ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+            ctx.fillStyle = cores_escolhidas_usuario[i];
+            ctx.fill();
+            ctx.closePath();
+          }
+        }
       });
     }
   });
 
-  // Função que retorna o índice da bolinha no vetor
-  function circleIndex(circle) {
-    if (
-      circle.x === posicao_bolinhas_grandes[0].x &&
-      circle.y === posicao_bolinhas_grandes[0].y
-    ) {
-      return 0;
-    } else if (
-      circle.x === posicao_bolinhas_grandes[1].x &&
-      circle.y === posicao_bolinhas_grandes[1].y
-    ) {
-      return 1;
-    } else if (
-      circle.x === posicao_bolinhas_grandes[2].x &&
-      circle.y === posicao_bolinhas_grandes[2].y
-    ) {
-      return 2;
-    } else if (
-      circle.x === posicao_bolinhas_grandes[3].x &&
-      circle.y === posicao_bolinhas_grandes[3].y
-    ) {
-      return 3;
-    }
-  }
+  btn.addEventListener("click", () => {
+    const primeira_linha = posicao_bolinhas_pequenas[0];
+    console.log(senha);
+    console.log(cores_escolhidas_usuario);
+    desenharBolinhasPequenas(primeira_linha, cores_escolhidas_usuario, senha);
+  });
 };
 
-var posicao_bolinhas_medias = [
+const posicao_bolinhas_medias = [
   /* linha 1 */
-  { x: 158, y: 646, radius: 9 },
-  { x: 212, y: 646, radius: 9 },
-  { x: 266, y: 646, radius: 9 },
-  { x: 320, y: 646, radius: 9 },
+  [
+    { x: 158, y: 646, radius: 9 },
+    { x: 212, y: 646, radius: 9 },
+    { x: 266, y: 646, radius: 9 },
+    { x: 320, y: 646, radius: 9 },
+  ],
   /* Linha 2 */
-  { x: 158, y: 586, radius: 9 },
-  { x: 212, y: 586, radius: 9 },
-  { x: 266, y: 586, radius: 9 },
-  { x: 320, y: 586, radius: 9 },
+  [
+    { x: 158, y: 586, radius: 9 },
+    { x: 212, y: 586, radius: 9 },
+    { x: 266, y: 586, radius: 9 },
+    { x: 320, y: 586, radius: 9 },
+  ],
   /* Linha 3 */
-  { x: 158, y: 526, radius: 9 },
-  { x: 212, y: 526, radius: 9 },
-  { x: 266, y: 526, radius: 9 },
-  { x: 320, y: 526, radius: 9 },
+  [
+    { x: 158, y: 526, radius: 9 },
+    { x: 212, y: 526, radius: 9 },
+    { x: 266, y: 526, radius: 9 },
+    { x: 320, y: 526, radius: 9 },
+  ],
   /* Linha 4 */
-  { x: 158, y: 466, radius: 9 },
-  { x: 212, y: 466, radius: 9 },
-  { x: 266, y: 466, radius: 9 },
-  { x: 320, y: 466, radius: 9 },
+  [
+    { x: 158, y: 466, radius: 9 },
+    { x: 212, y: 466, radius: 9 },
+    { x: 266, y: 466, radius: 9 },
+    { x: 320, y: 466, radius: 9 },
+  ],
   /* Linha 5 */
-  { x: 158, y: 466, radius: 9 },
-  { x: 212, y: 466, radius: 9 },
-  { x: 266, y: 466, radius: 9 },
-  { x: 320, y: 466, radius: 9 },
+  [
+    { x: 158, y: 466, radius: 9 },
+    { x: 212, y: 466, radius: 9 },
+    { x: 266, y: 466, radius: 9 },
+    { x: 320, y: 466, radius: 9 },
+  ],
   /* Linha 5 */
-  { x: 158, y: 406, radius: 9 },
-  { x: 212, y: 406, radius: 9 },
-  { x: 266, y: 406, radius: 9 },
-  { x: 320, y: 406 },
+  [
+    { x: 158, y: 406, radius: 9 },
+    { x: 212, y: 406, radius: 9 },
+    { x: 266, y: 406, radius: 9 },
+    { x: 320, y: 406, radius: 9 },
+  ],
   /* Linha 6 */
-  { x: 158, y: 346, radius: 9 },
-  { x: 212, y: 346, radius: 9 },
-  { x: 266, y: 346, radius: 9 },
-  { x: 320, y: 346, radius: 9 },
+  [
+    { x: 158, y: 346, radius: 9 },
+    { x: 212, y: 346, radius: 9 },
+    { x: 266, y: 346, radius: 9 },
+    { x: 320, y: 346, radius: 9 },
+  ],
   /* Linha 7 */
-  { x: 158, y: 286, radius: 9 },
-  { x: 212, y: 286, radius: 9 },
-  { x: 266, y: 286, radius: 9 },
-  { x: 320, y: 286, radius: 9 },
+  [
+    { x: 158, y: 286, radius: 9 },
+    { x: 212, y: 286, radius: 9 },
+    { x: 266, y: 286, radius: 9 },
+    { x: 320, y: 286, radius: 9 },
+  ],
   /* Linha 8 */
-  { x: 158, y: 226, radius: 9 },
-  { x: 212, y: 226, radius: 9 },
-  { x: 266, y: 226, radius: 9 },
-  { x: 320, y: 226, radius: 9 },
+  [
+    { x: 158, y: 226, radius: 9 },
+    { x: 212, y: 226, radius: 9 },
+    { x: 266, y: 226, radius: 9 },
+    { x: 320, y: 226, radius: 9 },
+  ],
 ];
 
 var posicao_bolinhas_pequenas = [
   /* Linha 1 */
-  { x: 79, y: 638, radius: 4 },
-  { x: 94, y: 638, radius: 4 },
-  { x: 79, y: 652, radius: 4 },
-  { x: 94, y: 652, radius: 4 },
+  [
+    { x: 79, y: 638, radius: 4 },
+    { x: 94, y: 638, radius: 4 },
+    { x: 79, y: 652, radius: 4 },
+    { x: 94, y: 652, radius: 4 },
+  ],
   /* Linha 2 */
-  { x: 79, y: 579, radius: 4 },
-  { x: 94, y: 579, radius: 4 },
-  { x: 79, y: 593, radius: 4 },
-  { x: 94, y: 593, radius: 4 },
+  [
+    { x: 79, y: 579, radius: 4 },
+    { x: 94, y: 579, radius: 4 },
+    { x: 79, y: 593, radius: 4 },
+    { x: 94, y: 593, radius: 4 },
+  ],
   /* Linha 3 */
-  { x: 79, y: 519, radius: 4 },
-  { x: 94, y: 519, radius: 4 },
-  { x: 79, y: 534, radius: 4 },
-  { x: 94, y: 534, radius: 4 },
+  [
+    { x: 79, y: 519, radius: 4 },
+    { x: 94, y: 519, radius: 4 },
+    { x: 79, y: 534, radius: 4 },
+    { x: 94, y: 534, radius: 4 },
+  ],
   /* Linha 4 */
-  { x: 79, y: 459, radius: 4 },
-  { x: 94, y: 459, radius: 4 },
-  { x: 79, y: 474, radius: 4 },
-  { x: 94, y: 474, radius: 4 },
+  [
+    { x: 79, y: 459, radius: 4 },
+    { x: 94, y: 459, radius: 4 },
+    { x: 79, y: 474, radius: 4 },
+    { x: 94, y: 474, radius: 4 },
+  ],
   /* Linha 5 */
-  { x: 79, y: 399, radius: 4 },
-  { x: 94, y: 399, radius: 4 },
-  { x: 79, y: 414, radius: 4 },
-  { x: 94, y: 414, radius: 4 },
+  [
+    { x: 79, y: 399, radius: 4 },
+    { x: 94, y: 399, radius: 4 },
+    { x: 79, y: 414, radius: 4 },
+    { x: 94, y: 414, radius: 4 },
+  ],
   /* Linha 6 */
-  { x: 79, y: 339, radius: 4 },
-  { x: 94, y: 339, radius: 4 },
-  { x: 79, y: 354, radius: 4 },
-  { x: 94, y: 354, radius: 4 },
+  [
+    { x: 79, y: 339, radius: 4 },
+    { x: 94, y: 339, radius: 4 },
+    { x: 79, y: 354, radius: 4 },
+    { x: 94, y: 354, radius: 4 },
+  ],
   /* Linha 7 */
-  { x: 79, y: 279, radius: 4 },
-  { x: 94, y: 279, radius: 4 },
-  { x: 79, y: 294, radius: 4 },
-  { x: 94, y: 294, radius: 4 },
+  [
+    { x: 79, y: 279, radius: 4 },
+    { x: 94, y: 279, radius: 4 },
+    { x: 79, y: 294, radius: 4 },
+    { x: 94, y: 294, radius: 4 },
+  ],
   /* linha 8 */
-  { x: 79, y: 219, radius: 4 },
-  { x: 94, y: 219, radius: 4 },
-  { x: 79, y: 234, radius: 4 },
-  { x: 94, y: 234, radius: 4 },
+  [
+    { x: 79, y: 219, radius: 4 },
+    { x: 94, y: 219, radius: 4 },
+    { x: 79, y: 234, radius: 4 },
+    { x: 94, y: 234, radius: 4 },
+  ],
 ];
